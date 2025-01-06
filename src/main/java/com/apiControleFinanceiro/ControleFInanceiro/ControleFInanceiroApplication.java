96ce5e1d9715828e3a199374ff23ce9385d08abd
Luiz Henrique Fernandes da Silva <91839356+luiz-henrique-f@users.noreply.github.com>
2025-01-05 22:42:46 -0300

    first commit

---
+++ src/main/java/com/apiControleFinanceiro/ControleFInanceiro/ControleFInanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.apiControleFinanceiro.ControleFInanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFInanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFInanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiControleFinanceiro/ControleFInanceiro/ControleFInanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiControleFinanceiro.ControleFInanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFInanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
